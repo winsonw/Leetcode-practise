a84107c8dc6f836a95c8cd3c2c66526e8b293382
Ziwei Wang <ziwei.1.wang@kcl.ac.uk>
2023-02-24 10:04:01 +0800

    merge intervals not using hash table

--- Google/Sorting_and_Searching/Merge_Intervals.py
+++ Google/Sorting_and_Searching/Merge_Intervals.py
@@ -1,30 +1,21 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        hash_table = [0] * 10001
-        for left, right in intervals:
-            while left < right:
-                hash_table[left] = 1
-                left += 1
-            if hash_table[right] == 0:
-                hash_table[right] = 2
-                
-                
-        
-        index = 0
+        intervals.sort()
+        l = len(intervals)
         new_intervals = []
-        while index < 10001:
-            left = index
-            while hash_table[index] == 1:
-                index += 1
+        i = 0
+        
+        while i < l:
+            left = intervals[i][0]
+            right = intervals[i][1]
+            i += 1
+            while i < l and (left <= intervals[i][0] <= right or left <= intervals[i][1] <= right):
+                if intervals[i][0] < left:
+                    left = intervals[i][0]
+                if intervals[i][1] > right:
+                    right = intervals[i][1]
+                i += 1
+            new_intervals.append([left, right])
             
-            if hash_table[index] == 2:
-                index += 1
-            if index != left:
-                new_intervals.append([left, index-1])
-            else:
-                index += 1
-        
-        print(hash_table)
-        
         return new_intervals
             
